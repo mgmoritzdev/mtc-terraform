374e53e293693b132dcf633ea6fb40654c48f5c8
Marcos Moritz <moritz@portaltelemedicina.com.br>
2021-11-14 00:53:17 -0300

    Add docker container resource

--- moritz/main.tf
+++ moritz/main.tf
@@ -17,7 +17,11 @@ resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
-# resource "docker_container" "foo" {
-#   image = docker_image.ubuntu.latest
-#   name = "foo"
-# }
+resource "docker_container" "nodered_container" {
+  name = "nodered"
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = 1880
+    external = 1880
+  }
+}
