d3306d2a8d80e670f7f1919a2965244b8d14abfb
Marcos Moritz <moritz@portaltelemedicina.com.br>
2021-11-14 02:26:22 -0300

    Add a random part of container name

--- moritz/main.tf
+++ moritz/main.tf
@@ -13,12 +13,18 @@ provider "docker" {
   # host = "unix:///var/run/docker.sock"
 }
 
+resource "random_string" "random" {
+  length = 4
+  special = false
+  upper = false
+}
+
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
 resource "docker_container" "nodered_container" {
-  name = "nodered"
+  name = join("-", ["nodered", random_string.random.result])
   image = docker_image.nodered_image.latest
   ports {
     internal = 1880
