11ffedecf17a0b4e80f185e1d6e117da1f3761c2
Marcos Moritz <moritz@portaltelemedicina.com.br>
2021-11-16 00:35:02 -0300

    Use variables

--- moritz/main.tf
+++ moritz/main.tf
@@ -13,8 +13,29 @@ provider "docker" {
   # host = "unix:///var/run/docker.sock"
 }
 
-resource "random_string" "random" {
-  count   = 2
+variable nodered_cointainer_internal_port {
+  type    = number
+  default = 1880
+}
+
+variable nodered_container_count {
+  type    = number
+  default = 2
+
+  validation {
+    condition = var.nodered_container_count >= 0
+    error_message = "The number of containers must be positive."
+  }
+}
+
+resource "random_integer" "nodered_container_external_port" {
+  count = var.nodered_container_count
+  min   = 8000
+  max   = pow(2, 16) - 1
+}
+
+resource "random_string" "nodered_container_sufix" {
+  count   = var.nodered_container_count
   length  = 4
   special = false
   upper   = false
@@ -25,11 +46,12 @@ resource "docker_image" "nodered_image" {
 }
 
 resource "docker_container" "nodered_container" {
-  count = 1
-  name  = join("-", ["nodered", random_string.random[count.index].result])
+  count = var.nodered_container_count
+  name  = join("-", ["nodered", random_string.nodered_container_sufix[count.index].result])
   image = docker_image.nodered_image.latest
   ports {
-    internal = 1880
+    internal = var.nodered_cointainer_internal_port
+    external = random_integer.nodered_container_external_port[count.index].result
   }
 }
 
