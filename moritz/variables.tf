55f219418354661c09fa0a62c28375c06c227b0d
Marcos Moritz <moritz@portaltelemedicina.com.br>
2022-06-28 02:51:53 -0300

    move locals to a file

--- moritz/variables.tf
+++ moritz/variables.tf
@@ -28,29 +28,3 @@ variable secret {
   type      = string
   sensitive = true
 }
-
-locals {
-  deployment = {
-    nodered = {
-      container_count =  length(var.external_ports["nodered"][terraform.workspace])
-      image = var.images["nodered"][terraform.workspace]
-      internal_port = var.internal_ports.nodered
-      external_port = var.external_ports.nodered[terraform.workspace]
-      container_path = "/data"
-    }
-    influxdb = {
-      container_count =  length(var.external_ports["influxdb"][terraform.workspace])
-      image = var.images["influxdb"][terraform.workspace]
-      internal_port = var.internal_ports.influxdb
-      external_port = var.external_ports.influxdb[terraform.workspace]
-      container_path = "/var/lib/influxdb"
-    }
-    grafana = {
-      container_count =  length(var.external_ports["grafana"][terraform.workspace])
-      image = var.images["grafana"][terraform.workspace]
-      internal_port = var.internal_ports.grafana
-      external_port = var.external_ports.grafana[terraform.workspace]
-      container_path = "/var/lib/grafana"
-    }
-  }
-}
